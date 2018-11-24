4f8418a74e2f836c1d63f0a8c9db47e845b0325d
Egor Dovnar <egordovnar.job@gmail.com>
2018-11-24 19:37:29 +0300

    Initial commit. Created some basic structure, imported array_rotator.rb, initialized rspec, created spec_helper, added Gemfile, wrote some basic tests for ArrayRotator class

---
+++ lib/array_rotator.rb
@@ -0,0 +1,18 @@
+class ArrayRotator
+
+  attr_accessor :array
+  def initialize(array)
+    @array = array.clone
+  end
+
+  def rotate(k_times)
+    k = k_times.to_i
+    if k.positive?
+      first = k % @array.length
+      @array = @array.slice!(-first, @array.length).concat(@array)
+    else
+      raise ArgumentError, 'Zero or negative argument given, positive k expected.'
+    end
+  end
+
+end
---
+++ spec/array_rotator_spec.rb
@@ -0,0 +1,40 @@
+$LOAD_PATH.unshift(File.expand_path('../../lib', __dir__))
+
+require 'spec_helper'
+require './lib/array_rotator'
+
+RSpec.describe ArrayRotator, '#rotate' do
+  let(:array) { Array.new([1, 2, 3, 4, 5, 6, 7, 8, 9]) }
+  let(:rotator) { ArrayRotator.new(array) }
+
+  context 'when given a correct k value' do
+    it 'should rotate the given array to k (k < array.length) positions to the right' do
+      expect(rotator.rotate(4)).to eq([6, 7, 8, 9, 1, 2, 3, 4, 5])
+    end
+
+    it 'should rotate the given array to k (k > array.length) positions to the right' do
+      expect(rotator.rotate(10)).to eq([9, 1, 2, 3, 4, 5, 6, 7, 8])
+    end
+  end
+
+  context 'when given an incorrect k value' do
+    it 'should raise an ArgumentError if k == 0' do
+      expect { rotator.rotate(0) }.to raise_error(ArgumentError)
+    end
+
+    it 'should raise an ArgumentError if k < 0' do
+      expect { rotator.rotate(-5) }.to raise_error(ArgumentError)
+    end
+  end
+
+  context 'when reusing the same ArrayRotator with another array' do
+    it 'should return the correct result' do
+      rotator.rotate(4)
+      another_array = [6, 3, 4, 5]
+      rotator.array = another_array
+      another_rotator = ArrayRotator.new(another_array)
+      expect(rotator.rotate(3)).to eq(another_rotator.rotate(3))
+    end
+  end
+
+end
